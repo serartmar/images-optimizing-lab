48d32c16f60abe5be2c1d5a796ec1e472b1339e0
Artem Marochkanych <serartmar@gmail.com>
2019-01-28 12:28:51 +0200

    Beautify gulpfile.js with Prettier

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,32 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
-const cache = require('gulp-cache');
-const del = require('del');
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const cache = require("gulp-cache");
+const del = require("del");
 
 // Optimize images with no-repeating
-gulp.task('images', function(){
-  return gulp.src('todo/*.+(png|JPG|jpg|jpeg|gif|svg)')
-  // Cache images that were already optimized with imagemin
-  .pipe(cache(imagemin({
-      interlaced: true,
-      progressive: true,
-      verbose: true
-    })))
-  .pipe(gulp.dest('done'))
+gulp.task("images", function() {
+  return (
+    gulp
+      .src("todo/*.+(png|JPG|jpg|jpeg|gif|svg)")
+      // Cache images that were already optimized with imagemin
+      .pipe(
+        cache(
+          imagemin({
+            interlaced: true,
+            progressive: true,
+            verbose: true
+          })
+        )
+      )
+      .pipe(gulp.dest("done"))
+  );
 });
 
 // Clean up build folder
-gulp.task('clean', function(){
-  return del('done');
+gulp.task("clean", function() {
+  return del("done");
 });
 
-gulp.task('default', gulp.series('clean', 'images')
-);
+gulp.task("default", gulp.series("clean", "images"));
